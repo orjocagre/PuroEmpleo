54e060c1f162c4fef3ae5aad7b2c06aefcc4a08c
Ethel Videa <100855553+EthelVidea20@users.noreply.github.com>
2023-05-17 17:45:24 -0600

    dimensiones prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 document.getElementById("back_menu").addEventListener("click", ocultar_menu);
 nav = document.getElementById("nav");
 background_menu=document.getElementById("back_menu");
-
+main=document.querySelector("main");
 function mostrar_menu(){
     nav.style.right = "0px";
     background_menu.style.display = "block";
+   /* main.style.backgroundColor= "#1F2B0B";*/
 }
 
 function ocultar_menu(){
@@ -13,5 +14,6 @@ function ocultar_menu(){
     nav.style.right = "-250px";
     background_menu.style.display = "none";
 
+    
 }
 
