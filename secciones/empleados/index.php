ff5149404caa6ccf9b649f3bbefb5665a77e3074
NoeHernandez11 <100396482+NoeHernandez11@users.noreply.github.com>
2023-06-11 23:41:57 -0600

    reordenaje y termino de apartado puestos

--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,8 +1,32 @@
-<?php include("../../templates/header.php"); ?>
+<!doctype html>
+<html lang="en">
 
-<br/>
-<h4>Empleados</h4>
-<div class="card">
+<head>
+  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Empleados</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/header_footer.css">
+
+</head>
+
+<body>
+  <header>
+  <?php include("../../vista/header.php"); ?>
+
+  </header>
+
+  <main class="container">
+
+  <br/>
+
+  <h4>Empleados</h4>
+
+  <div class="card">
     <div class="card-header">
         
         <a name="" id="" class="btn btn-primary" 
@@ -44,6 +68,23 @@
     
     </div>
     
+
 </div>
 
-<?php include("../../templates/footer.php"); ?>
+  </main>
+
+  <footer>
+  <?php include("../../vista/footer.php"); ?>
+
+  </footer>
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
--- secciones/puestos/crear.php
+++ secciones/puestos/crear.php
@@ -1,7 +1,44 @@
-<?php include("../../templates/header.php"); ?>
+<!doctype html>
+<html lang="en">
 
-<br/>
-<div class="card">
+<head>
+  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Puestos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/header_footer.css">
+
+</head>
+
+<body>
+<?php include("../../config.php");
+
+if($_POST){
+  //recoleccion de datos mediante post
+   $nombredelpuesto=(isset($_POST["nombredelpuesto"])?$_POST["nombredelpuesto"]:"");
+   //preparar la inserccion de los datos
+   $sentencia=$conexion->prepare("INSERT INTO puesto(id,nombredelpuesto)
+            VALUES (default, :nombredelpuesto)");
+   //asignacion de valores que vienen de post(formukario)
+  $sentencia->bindParam(":nombredelpuesto",$nombredelpuesto);
+  $sentencia->execute();
+  header("Location:index.php");
+}  
+?>
+  <header>
+  <?php include("../../vista/header.php"); ?>
+
+  </header>
+
+  <main class="container">
+
+  <br/>
+
+  <div class="card">
     <div class="card-header">
         Puestos
     </div>
@@ -21,7 +58,22 @@
         </form>
     </div>
     <div class="card-footer text-muted"> </div>
-</div>
+    </div>
 
+  </main>
 
-<?php include("../../templates/footer.php"); ?>
+  <footer>
+  <?php include("../../vista/footer.php"); ?>
+
+  </footer>
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
