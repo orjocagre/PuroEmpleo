0b43bc4636b1d8a4616ef1bce884ce9dccabefe4
orlando <orlandocaceres114@gmail.com>
2024-01-28 09:52:41 -0600

    cambio conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,47 +1,52 @@
 <?php
-class CConexion {
-    public static function ConexionBD() {
-        $host = "b7wh7en78fhwxbtfds8b-postgresql.services.clever-cloud.com";
-        $dbname = "b7wh7en78fhwxbtfds8b";
-        $username="uqwv3xwcxq45cskcb1xy";
-        $password="8G7wBLZqK1tIq84xELRiVIzNsdcNxL";
+class CConexion
+{
+    public static function ConexionBD()
+    {
 
-        try{
+
+        // $host = "b7wh7en78fhwxbtfds8b-postgresql.services.clever-cloud.com";
+        $host = "bepbpehtkmlvwptycspk-postgresql.services.clever-cloud.com";
+        // $dbname = "b7wh7en78fhwxbtfds8b";
+        $dbname = "bepbpehtkmlvwptycspk";
+        $username = "uqwv3xwcxq45cskcb1xy";
+        $password = "8G7wBLZqK1tIq84xELRiVIzNsdcNxL";
+        $port = "50013";
+
+
+
+        try {
             //$conexion=new PDO("postgre:host=$servidor;dbname=album", $usuario, $contrasenia);
-            $conn=new PDO("pgsql:host=$host; dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);            
-        }
-        catch(PDOException $error){
-            //echo "conexion erronea".$error;
+            $conn = new PDO("pgsql:host=$host; port=$port; dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $error) {
             return null;
         }
         return $conn;
     }
 
 
-    public static function buscarBD($sql) {
+    public static function buscarBD($sql)
+    {
         $conexion = self::ConexionBD();
-        if($conexion != null) {
+        if ($conexion != null) {
             $sentencia = $conexion->prepare($sql);
             $sentencia->execute();
             $resultado = $sentencia->fetchAll();
             return $resultado;
-        }
-        else {
+        } else {
             return null;
         }
     }
 
-    public static function insertarBD($sql) {
+    public static function insertarBD($sql)
+    {
         $conexion = self::ConexionBD();
-        if($conexion != null) {
+        if ($conexion != null) {
             $conexion->exec($sql);
             return $conexion->lastInsertId();
-        }
-        else {
+        } else {
             return 0;
         }
     }
-
 }
-?>
